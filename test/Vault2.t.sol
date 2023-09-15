bf4dca92516d5d4e8e491f5a62099b6e3d2d8830
Juan_Fuente_T <jfuentet@gmail.com>
2023-09-15 21:16:36 +0200

    Vault2 15-09-23

--- test/Vault2.t.sol
+++ test/Vault2.t.sol
@@ -1,14 +1,37 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.11;
 
-
-import "forge-std/Test.sol";
+import "lib/forge-std/src/Test.sol";
 import "../src/Vault2.sol";
 
 contract Vault2Test is Test {
     Vault2 public vault;
+    address public Juan;
 
     function setUp() public {
         vault = new Vault2{value: 0.0001 ether}();
+        Juan = makeAddr("Juan");
+    }
+
+    function testVault2() public {
+        vm.startPrank(Juan);
+        console.log("Balance Vault: ", address(vault).balance);
+
+        //(bool sent, ) = payable(address(vault)).call{value:0.0001 ether}("");
+        //require(sent, "Error en el call");
+
+        //console.log("Balance Vault despues: ",address(vault).balance);
+        console.log("Balance Juan: ", Juan.balance);
+
+        address payable addr = payable(address(vault));
+        //payable(address(vault)).call{value: 0.0001 ether}("");
+
+        selfdestruct(addr);
+        
+        assertEq(Juan.balance, 0.001 ether);
+        console.log("Balance Juan: ", Juan.balance);
+        console.log("Balance Vault: ", address(vault).balance);
+        
     }
 }
+
